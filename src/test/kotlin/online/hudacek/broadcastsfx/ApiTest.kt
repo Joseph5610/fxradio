5b772b72f9d29467ce7fe6a9e08e8d338ec561f4
jozef <jozo5610@gmail.com>
2020-05-24 00:42:30 +0200

    add missing changes from master to develop

---
+++ src/test/kotlin/online/hudacek/broadcastsfx/ApiTest.kt
@@ -0,0 +1,49 @@
+/*
+ *  Copyright 2020 FXRadio by hudacek.online
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package online.hudacek.broadcastsfx
+
+import javafx.stage.Stage
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.testfx.framework.junit5.ApplicationExtension
+import org.testfx.framework.junit5.Start
+
+@ExtendWith(ApplicationExtension::class)
+class ApiTest {
+
+    private lateinit var app: Broadcasts
+
+    @Start
+    fun start(stage: Stage) {
+        app = Broadcasts()
+        app.start(stage)
+    }
+
+    @Test
+    fun testTopStations() {
+        StationsApi.client
+                .getTopStations()
+                .subscribe { stations ->
+                    Assertions.assertEquals(50, stations.size)
+                    stations.forEach {
+                        //top 50 stations should not have empty URL
+                        Assertions.assertTrue(it.url_resolved != null)
+                    }
+                }.dispose()
+    }
+}
