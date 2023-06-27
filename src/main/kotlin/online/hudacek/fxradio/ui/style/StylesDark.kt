354007db54b629d7e3e8615ca760c91bb66a8ce8
jozef <jozo5610@gmail.com>
2023-06-27 21:57:12 +0200

    Release 0.17

--- src/main/kotlin/online/hudacek/fxradio/ui/style/StylesDark.kt
+++ src/main/kotlin/online/hudacek/fxradio/ui/style/StylesDark.kt
@@ -574,8 +574,8 @@ class StylesDark : Stylesheet() {
             }
         }
 
-
         InternalWindow.Styles.floatingWindowWrapper {
+
             InternalWindow.Styles.window {
                 InternalWindow.Styles.top {
                     backgroundRadius += box(6.px, 6.px, 0.px, 0.px)
